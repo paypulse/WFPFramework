d3301c2af0d3352c1716012463c9b796bfd97f3e
paypulse <sourcedev5@gmail.com>
2017-11-03 21:16:06 +0900

    Basic FrameWork

--- WPFrameWork/WPFrameWork/WPFrameWork.cs
+++ WPFrameWork/WPFrameWork/WPFrameWork.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
 
 namespace WPFrameWork
 {
@@ -12,6 +12,14 @@ namespace WPFrameWork
         [STAThread]
         public static void Main()
         {
+            Application app = new Application();
+
+            Uri uri = new Uri("pack://application:,,,/Setting.xml");
+            Stream stream = Application.GetResourceStream(uri).Stream;
+            Window win = XamlReader.Load(stream) as Window;
+
+            //Event 추가시 
+            //win.AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick));
 
         }
     }
